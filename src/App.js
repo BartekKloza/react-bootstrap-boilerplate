66d4c998ed9352ca94240c9a5f0b9c645530b3e4
Bartlomiej Kloza <bartlomiej.kloza@bbc.co.uk>
2019-10-04 16:15:44 +0100

    added couple of stuff ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './Home';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Header />
+      <Route exact path="/" component={Home} />
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello Home.js</h1>
+            </div>
+        )
+    }
+}
